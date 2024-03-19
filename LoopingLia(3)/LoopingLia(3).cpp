d994f77681c68789a50434f26a5aabb7d457eec4
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-03-19 11:27:25 +0700

    Prosedur & Fungsi (Input Data Nilai Panjang & Lebar)

---
+++ LoopingLia(3)/LoopingLia(3).cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+
+int panjang, lebar;
+
+void inputdata() {
+	cout << "Masukkan Nilai Panjang : ";
+	cin >> panjang;
+	cout << "Masukkan Nilai Lebar : ";
+	cin >> lebar;
+
+}
+
+
+
+	
